4b1fad0bc670144cc4ace37970493f8ca6fee58c
tmy628 <tmy6h1r@gmail.com>
2021-05-07 06:01:52 +0000

    deviseのcontrollerのcreateアクションをオーバーライドして、ThanksMailerを呼び出す

---
+++ bookers2/app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def create
+    #スーパークラス(devise)のcreateアクションを呼ぶ
+    super
+    #ThanksMailerクラスのsend_when_signupメソッドを呼び、POSTから受け取ったuserのemailとnameを渡す
+    ThanksMailer.send_when_signup(params[:user][:email],params[:user][:name]).deliver
+  end
+
+end
