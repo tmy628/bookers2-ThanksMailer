2dc7a084d0e21f6dbbb141192f66d7f682a7b404
tmy628 <tmy6h1r@gmail.com>
2021-05-07 06:04:21 +0000

    Revert "deviseのcontrollerのcreateアクションをオーバーライドして、ThanksMailerを呼び出す"
    This reverts commit 4b1fad0bc670144cc4ace37970493f8ca6fee58c.

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'search/search'
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users
 
   root :to => "homes#top"
   get "home/about" => "homes#about"
